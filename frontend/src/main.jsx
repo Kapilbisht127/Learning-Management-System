ea7677716a0033c3ccdb9f511a646c84a5af6e8d
kapilbisht127 <bkapil978@gmail.com>
2026-03-27 19:40:11 +0530

    LMS project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom";
+import { ClerkProvider } from '@clerk/react'
+
+//import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!PUBLISHABLE_KEY){
+  throw new Error("Add your Clerk Publishable key to the .evn file");
+  
+}
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+  </ClerkProvider>
+
+  
+)
